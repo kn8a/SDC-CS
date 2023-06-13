c0912c7052e0e65c3b301ca5bdc0c64aedfaf629
kn8a <mydreamhomefl@gmail.com>
2023-06-13 04:42:50 -0400

    changed program to use hashmap instead of treemap

--- CS201 - Data Structures & Algorithms Assignment/Part 2 - Sorting & Searching HashMap/Main.java
+++ CS201 - Data Structures & Algorithms Assignment/Part 2 - Sorting & Searching HashMap/Main.java
@@ -17,134 +17,163 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        // Create a TreeMap to store the lowercase state names and their capital pairs
-        TreeMap<String, String> stateCapitalMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 
-        // Array containing the states and capitals
-        String[][] statesAndCapitals = {
-            // State and capital pairs
-            {"Alabama","Montgomery"},
-    			{"Alaska","Juneau"},
-    			{"Arizona","Phoenix"},
-    			{"Arkansas","Little Rock"},
-    			{"California","Sacramento"},
-    			{"Colorado","Denver"},
-    			{"Connecticut","Hartford"},
-    			{"Delaware","Dover"},
-    			{"Florida","Tallahassee"},
-    			{"Georgia","Atlanta"},
-    			{"Hawaii","Honolulu"},
-    			{"Idaho","Boise"},
-    			{"Illinois","Springfield"},
-    			{"Indiana","Indianapolis"},
-    			{"Iowa","Des Moines"},
-    			{"Kansas","Topeka"},
-    			{"Kentucky","Frankfort"},
-    			{"Louisiana","Baton Rouge"},
-    			{"Maine","Augusta"},
-    			{"Maryland","Annapolis"},
-    			{"Massachusetts","Boston"},
-    			{"Michigan","Lansing"},
-    			{"Minnesota","St. Paul"},
-    			{"Mississippi","Jackson"},
-    			{"Missouri","Jefferson City"},
-    			{"Montana","Helena"},
-    			{"Nebraska","Lincoln"},
-    			{"Nevada","Carson City"},
-    			{"New Hampshire","Concord"},
-    			{"New Jersey","Trenton"},
-    			{"New Mexico","Santa Fe"},
-    			{"New York","Albany"},
-    			{"North Carolina","Raleigh"},
-    			{"North Dakota","Bismarck"},
-    			{"Ohio","Columbus"},
-    			{"Oklahoma","Oklahoma City"},
-    			{"Oregon","Salem"},
-    			{"Pennsylvania","Harrisburg"},
-    			{"Rhode Island","Providence"},
-    			{"South Carolina","Columbia"},
-    			{"South Dakota","Pierre"},
-    			{"Tennessee","Nashville"},
-    			{"Texas","Austin"},
-    			{"Utah","Salt Lake City"},
-    			{"Vermont","Montpelier"},
-    			{"Virginia","Richmond"},
-    			{"Washington","Olympia"},
-    			{"West Virginia","Charleston"},
-    			{"Wisconsin","Madison"},
-    			{"Wyoming","Cheyenne"}
-        };
+  public static void main(String[] args) {
+    // Create a HashMap to store the case-insensitive state names and their capital pairs
+    HashMap<CaseInsensitiveString, String> stateCapitalMap = new HashMap<>();
 
-        // Add the lowercase state-capital pairs to the TreeMap
-        for (String[] pair : statesAndCapitals) {
-            // Extract the state and capital from the pair
-            String state = pair[0];
-            String capital = pair[1];
+    // Array containing the states and capitals
+    String[][] statesAndCapitals = {
+      // State and capital pairs
+      { "Alabama", "Montgomery" },
+      { "Alaska", "Juneau" },
+      { "Arizona", "Phoenix" },
+      { "Arkansas", "Little Rock" },
+      { "California", "Sacramento" },
+      { "Colorado", "Denver" },
+      { "Connecticut", "Hartford" },
+      { "Delaware", "Dover" },
+      { "Florida", "Tallahassee" },
+      { "Georgia", "Atlanta" },
+      { "Hawaii", "Honolulu" },
+      { "Idaho", "Boise" },
+      { "Illinois", "Springfield" },
+      { "Indiana", "Indianapolis" },
+      { "Iowa", "Des Moines" },
+      { "Kansas", "Topeka" },
+      { "Kentucky", "Frankfort" },
+      { "Louisiana", "Baton Rouge" },
+      { "Maine", "Augusta" },
+      { "Maryland", "Annapolis" },
+      { "Massachusetts", "Boston" },
+      { "Michigan", "Lansing" },
+      { "Minnesota", "St. Paul" },
+      { "Mississippi", "Jackson" },
+      { "Missouri", "Jefferson City" },
+      { "Montana", "Helena" },
+      { "Nebraska", "Lincoln" },
+      { "Nevada", "Carson City" },
+      { "New Hampshire", "Concord" },
+      { "New Jersey", "Trenton" },
+      { "New Mexico", "Santa Fe" },
+      { "New York", "Albany" },
+      { "North Carolina", "Raleigh" },
+      { "North Dakota", "Bismarck" },
+      { "Ohio", "Columbus" },
+      { "Oklahoma", "Oklahoma City" },
+      { "Oregon", "Salem" },
+      { "Pennsylvania", "Harrisburg" },
+      { "Rhode Island", "Providence" },
+      { "South Carolina", "Columbia" },
+      { "South Dakota", "Pierre" },
+      { "Tennessee", "Nashville" },
+      { "Texas", "Austin" },
+      { "Utah", "Salt Lake City" },
+      { "Vermont", "Montpelier" },
+      { "Virginia", "Richmond" },
+      { "Washington", "Olympia" },
+      { "West Virginia", "Charleston" },
+      { "Wisconsin", "Madison" },
+      { "Wyoming", "Cheyenne" },
+    };
 
-            // Add the state-capital pair to the TreeMap
-            stateCapitalMap.put(state, capital);
-        }
+    // Add the case-insensitive state-capital pairs to the HashMap
+    for (String[] pair : statesAndCapitals) {
+      // Extract the state and capital from the pair
+      String state = pair[0];
+      String capital = pair[1];
 
-        // Display the sorted Map
-        System.out.println("\nSorted Map:");
-        displayMap(stateCapitalMap);
+      // Create a CaseInsensitiveString object as the key
+      CaseInsensitiveString key = new CaseInsensitiveString(state);
 
-        // Prompt the user to enter a state until "exit" is entered
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("\nEnter a state (type 'exit' to exit): ");
-            String userInput = scanner.nextLine().trim();
-
-            // Check if the user wants to exit
-            if (userInput.equalsIgnoreCase("exit")) {
-                break;
-            }
-
-            // Standardize the user's input for case-insensitive search
-            String standardizedInput = capitalizeFirstLetter(userInput);
-
-            // Check if the state exists in the Map
-            if (stateCapitalMap.containsKey(standardizedInput)) {
-                // Retrieve the capital for the given state
-                String capital = stateCapitalMap.get(standardizedInput);
-
-                // Display the capital of the state
-                System.out.println("Capital of " + standardizedInput + ": " + capital);
-            } else {
-                System.out.println("Capital not found for " + userInput);
-            }
-        }
-
-        // Close the scanner
-        scanner.close();
+      // Add the state-capital pair to the HashMap
+      stateCapitalMap.put(key, capital);
     }
 
-    // Method to capitalize the first letter of each word in a string
-private static String capitalizeFirstLetter(String str) {
-    if (str == null || str.isEmpty()) {
-        return str;
-    }
-    // Split the string into words
-    String[] words = str.split(" ");
-    StringBuilder result = new StringBuilder();
-    for (String word : words) {
-        // Capitalize the first letter of each word
-        result.append(word.substring(0, 1).toUpperCase()).append(word.substring(1).toLowerCase()).append(" ");
-    }
-    // Remove the trailing space
-    return result.toString().trim();
-}
+    // Display the sorted Map
+    System.out.println("\nSorted Map:");
+    displayMap(stateCapitalMap);
 
-    // Method to display the content of the Map
-    private static void displayMap(Map<String, String> map) {
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            // Retrieve the state and capital from the Map entry
-            String state = entry.getKey();
-            String capital = entry.getValue();
+    // Prompt the user to enter a state until "exit" is entered
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      System.out.print("\nEnter a state (type 'exit' to exit): ");
+      String userInput = scanner.nextLine().trim();
 
-            // Display the state-capital pair
-            System.out.println(state + " - " + capital);
-        }
+      // Check if the user wants to exit
+      if (userInput.equalsIgnoreCase("exit")) {
+        break;
+      }
+
+      // Create a CaseInsensitiveString object from the user's input
+      CaseInsensitiveString userKey = new CaseInsensitiveString(userInput);
+
+      // Check if the state exists in the Map
+      if (stateCapitalMap.containsKey(userKey)) {
+        // Retrieve the capital for the given state
+        String capital = stateCapitalMap.get(userKey);
+
+        // Display the capital of the state
+        System.out.println("Capital of " + userKey.getValue() + ": " + capital);
+      } else {
+        System.out.println("Capital not found for " + userInput);
+      }
     }
+
+    // Close the scanner
+    scanner.close();
+  }
+
+  // Method to display the content of the Map
+  private static void displayMap(Map<CaseInsensitiveString, String> map) {
+    for (Map.Entry<CaseInsensitiveString, String> entry : map.entrySet()) {
+      // Retrieve the state and capital from the Map entry
+      CaseInsensitiveString state = entry.getKey();
+      String capital = entry.getValue();
+
+      // Display the state-capital pair
+      System.out.println(state.getValue() + " - " + capital);
+    }
+  }
+
+  // Custom class to represent a case-insensitive string
+  static class CaseInsensitiveString {
+
+    private final String value;
+
+    // Constructor to initialize the value
+    public CaseInsensitiveString(String value) {
+      this.value = value;
+    }
+
+    // Getter method to retrieve the value
+    public String getValue() {
+      return value;
+    }
+
+    // Override the equals() method for case-insensitive comparison
+    @Override
+    public boolean equals(Object obj) {
+      // Check if the references are the same
+      if (this == obj) {
+        return true;
+      }
+
+      // Check if the object is null or of different class
+      if (obj == null || getClass() != obj.getClass()) {
+        return false;
+      }
+
+      // Perform case-insensitive comparison
+      CaseInsensitiveString other = (CaseInsensitiveString) obj;
+      return value.equalsIgnoreCase(other.value);
+    }
+
+    // Override the hashCode() method for consistent hashing
+    @Override
+    public int hashCode() {
+      // Convert the value to lowercase and calculate the hash code
+      return value.toLowerCase().hashCode();
+    }
+  }
 }
